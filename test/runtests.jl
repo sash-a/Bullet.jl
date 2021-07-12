00f0940b96fddb6ae6912cdd187e7ae216836be4
sash-a <reallysasha@gmail.com>
2021-07-12 11:29:12 +0200

    Removed all cxx refs

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,3 @@ include("demo.jl")
 include("demo_robot.jl")
 include("robot_collision.jl")
 include("raycast.jl")
-
-
-import Bullet.BulletCxx: @cxxnew, @icxx_str, btVector3_ptr_to_array, btVector3_to_array
-__current_compiler__ =  Bullet.BulletCxx.Cxx.__default_compiler__ # used by Cxx macros
-
-include("cxx_simple.jl")
-include("cxx_bullet_c_api.jl")
-
-include("collision_flags.jl")
